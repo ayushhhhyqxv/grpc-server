c306c72c036cd14d546f368deed6db58036ff760
ayushhhhyqxv <ayushpadhaikar@gmail.com>
2025-09-12 22:45:31 +0530

    Added client and server files

---
+++ server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+	pb "github.com/ayushhhhyqxv/grpc-server"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type command struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server : %v",err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.registerGreetServiceServer(grpcServer,&command{})
+	if err := grpcServer.Serve(lis);err!=nil{
+		log.Fatalf("Failed to Start : %v",err)
+	}
+
+}
